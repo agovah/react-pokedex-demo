098d7dc014790bd42ae71bbee3ee30907d48e0f8
Nicolas S <sir.ago@gmail.com>
2020-10-18 16:45:47 +0200

    Updates

---
+++ precache-manifest.7d9718747a1aca7122fc833a3a804960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43a88ae33c5355920a7ae67325d3ddf",
+    "url": "/react-pokedex-demo/index.html"
+  },
+  {
+    "revision": "54b84d4a7ba9d2052bcc",
+    "url": "/react-pokedex-demo/static/css/main.41f8c624.chunk.css"
+  },
+  {
+    "revision": "650122b054ef711686ac",
+    "url": "/react-pokedex-demo/static/js/2.96ee14a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-pokedex-demo/static/js/2.96ee14a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b84d4a7ba9d2052bcc",
+    "url": "/react-pokedex-demo/static/js/main.669e7e94.chunk.js"
+  },
+  {
+    "revision": "0c2b463c6beb38e65c1f",
+    "url": "/react-pokedex-demo/static/js/runtime-main.ea0b3fe9.js"
+  }
+]);
